bc569a8ebf1893b46503a6d119ab2b617063ddc9
Irish1986 <13018674+Irish1986@users.noreply.github.com>
2020-12-27 13:10:30 -0500

    Create test_hello.py

---
+++ tests/test_hello.py
@@ -0,0 +1,12 @@
+__author__ = 'irish1986'
+
+import unittest
+from Hello import Greeter
+
+class MyTestCase(unittest.TestCase):
+    def test_default_greeting_set(self):
+        greeter = Greeter()
+        self.assertEqual(greeter.message, 'Hello world!')
+
+if __name__ == '__main__':
+    unittest.main()
